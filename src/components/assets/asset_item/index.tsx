f769f1ffc0d0c5c7bbfe5273c7c95103b32b1eb8
bagidea <thanawat@nexhe.com>
2022-02-19 05:03:49 +0700

    create drag asset and show mini image.

--- src/components/assets/asset_item/index.tsx
+++ src/components/assets/asset_item/index.tsx
@@ -1,7 +1,9 @@
 import {
+    Box,
     Flex,
     Image,
     Text,
+    useBoolean,
     VStack
 } from "@chakra-ui/react"
 
@@ -12,6 +14,8 @@ import {
 
 import { Asset } from "../../../redux/slices/context_3d"
 import { SceneCanvas } from "../../display/scene/canvas"
+import Draggable from "react-draggable"
+import { useState } from "react"
 
 const AssetItem: React.FC<{
         asset: Asset,
@@ -21,11 +25,21 @@ const AssetItem: React.FC<{
         //setSelected: Dispatch<SetStateAction<number>>
     }> = ({ asset, scene, checkSelect, /*lastSelected, setSelected*/ }) =>
 {
+    const [is_drag, setDrag] = useBoolean(false)
+    const [top_value, setTopValue] = useState("0px")
+    const [left_value, setLeftValue] = useState("0px")
+
     const selectAsset = (e: MouseEvent) => {
         e.stopPropagation()
         //setSelected(asset.index)
         checkSelect()
         scene.selectAsset(asset)
+    }
+
+    const onDragAsset = () => {
+    }
+
+    const offDragAsset = () => {
     }
 
     return (
@@ -34,8 +48,11 @@ const AssetItem: React.FC<{
             spacing="5px"
             cursor="pointer"
             onClick={ (e) => selectAsset(e as any) }
+            onMouseDown={ () => onDragAsset() }
+            onMouseUp={ () => offDragAsset() }
         >
             <Flex
+                position={ is_drag ? "static" : "relative" }
                 w="100px"
                 h="100px"
                 bgColor={ asset.isSelect ? "blue.800" : "gray.800" }
@@ -47,6 +64,47 @@ const AssetItem: React.FC<{
                     w="100px"
                     h="100px"
                     draggable={ false }
+                />
+
+                <Draggable
+                    position={ { x: 0, y: 0 } }
+                    onStart={ (e: MouseEvent) => {
+                            setDrag.on()
+                            setLeftValue((e.clientX - 40)+"px")
+                            setTopValue((e.clientY + 5)+"px")
+                        }
+                    }
+                    onDrag={ (e: MouseEvent) => {
+                            setLeftValue((e.clientX - 40)+"px")
+                            setTopValue((e.clientY + 5)+"px")
+                        }
+                    }
+                    onStop={ () => {
+                            setDrag.off()
+                        }
+                    }
+                >
+                    <Box
+                        position="absolute"
+                        w="100px"
+                        h="100px"
+                        top="0px"
+                        left="0px"
+                        pointerEvents={ is_drag ? "none" : "auto" }
+                    />
+                </Draggable>
+
+                <Box
+                    hidden={ !is_drag }
+                    userSelect="none"
+                    position="absolute"
+                    w="40px"
+                    h="40px"
+                    top={ top_value }
+                    left={ left_value }
+                    bgImage={ asset.pic }
+                    backgroundSize="contain"
+                    pointerEvents="none"
                 />
             </Flex>
 
